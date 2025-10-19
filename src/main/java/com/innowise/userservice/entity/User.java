1626472592f57c470236a58ac35753b51a0ab151
MegaXRL <zaxarrim@gmail.com>
2025-10-19 16:06:55 +0300

    Create entities, tables and indexes

---
+++ src/main/java/com/innowise/userservice/entity/User.java
@@ -0,0 +1,34 @@
+package com.innowise.userservice.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDate;
+import java.util.List;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private String surname;
+
+    @Column(name = "birth_date")
+    private LocalDate birthDate;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+    private List<CardInfo> cards;
+}
